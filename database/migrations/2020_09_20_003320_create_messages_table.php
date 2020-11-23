0387ef74c3ea07f577e2b80def4bcf8c91ee7bc7
unknown <momokaram223@gmail.com>
2020-11-23 22:28:21 +0200

    =migrate

--- database/migrations/2020_09_20_003320_create_messages_table.php
+++ database/migrations/2020_09_20_003320_create_messages_table.php
@@ -16,8 +16,12 @@ class CreateMessagesTable extends Migration
         Schema::create('messages', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('from_user');
-            $table->unsignedInteger('to_user');
-            $table->text('content');
+            $table->unsignedInteger('to_user')->nullable();
+            $table->text('content')->nullable();
+            $table->string('record')->nullable();
+            $table->string('files')->nullable();
+            $table->tinyInteger('clear_chat_user1')->default(0);
+            $table->tinyInteger('clear_chat_user2')->default(0);
             $table->timestamps();
         });
     }
